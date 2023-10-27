7452b5017316488e46fc9980f8f3eee8c663a8be
Cristian Pallarés <cristianp@wing.cloud>
2023-10-27 20:08:07 +0200

    feat: add turbo component

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ export * from "./node-project";
 export * from "./project";
 export * from "./semver";
 export * from "./tsup";
+export * from "./turbo";
 export * from "./typescript-config";
 export * from "./typescript-project";
 export * from "./vitest";
