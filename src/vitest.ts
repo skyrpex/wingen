c37338ced8f86624bba76b68b85987682204b068
Cristian Pallarés <cristian@pallares.pro>
2024-07-04 12:02:55 +0200

    fix: vitest workspace task runs along with every subproject's vitest task (#335)

--- src/vitest.ts
+++ src/vitest.ts
@@ -1,13 +1,14 @@
 import { Component, SourceCode } from "projen";
 import { Tsup } from "./tsup.js";
 import { TypescriptProject } from "./typescript-project";
+import { VitestWorkspace } from "./vitest-workspace.js";
 
 export class Vitest extends Component {
   constructor(project: TypescriptProject) {
     super(project);
 
     project.addDevDeps("vitest");
-    project.addScript("test", "vitest run --passWithNoTests");
+    project.testTask.exec("vitest run --passWithNoTests");
 
     const config = new SourceCode(project, "vitest.config.ts");
     config.line(`/* eslint-disable unicorn/no-abusive-eslint-disable */`);
@@ -26,6 +27,16 @@ export class Vitest extends Component {
   }
 
   preSynthesize(): void {
+    // There's no need to define a local test task if there's a vitest workspace at the root.
+    // The root's test task will use the vitest workspace and run tests for all subprojects.
+    const vitestWorkspace = this.project.root.components.find(
+      (component) => component instanceof VitestWorkspace,
+    ) as VitestWorkspace | undefined;
+    if (vitestWorkspace) {
+      this.project.testTask.reset();
+    }
+
+    // Allow tsup, if exists, to treeshake the import.meta.vitest blocks in source files.
     const tsup = this.project.components.find(
       (component) => component instanceof Tsup,
     ) as Tsup | undefined;
