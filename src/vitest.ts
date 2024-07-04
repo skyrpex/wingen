0d67bcc1934699e368e27cd20f37312ea4f8f44c
Cristian Pallarés <cristian@pallares.pro>
2024-07-04 20:04:01 +0200

    feat: change vitest behavior (#336)

--- src/vitest.ts
+++ src/vitest.ts
@@ -1,23 +1,35 @@
 import { Component, SourceCode } from "projen";
 import { Tsup } from "./tsup.js";
 import { TypescriptProject } from "./typescript-project";
-import { VitestWorkspace } from "./vitest-workspace.js";
 
 export class Vitest extends Component {
   constructor(project: TypescriptProject) {
     super(project);
 
-    project.addDevDeps("vitest");
-    project.testTask.exec("vitest run --passWithNoTests");
+    project.addDevDeps("vitest", "@vitest/coverage-v8");
+    project.testTask.exec("pnpm exec vitest run --passWithNoTests");
+
+    project.addTask("test-coverage", {
+      exec: "pnpm exec vitest run --passWithNoTests --coverage",
+    });
+
+    project.addGitIgnore("/coverage/");
 
     const config = new SourceCode(project, "vitest.config.ts");
     config.line(`/* eslint-disable unicorn/no-abusive-eslint-disable */`);
     config.line(`/* eslint-disable */`);
-    config.line(`import { defineConfig } from "vitest/config";`);
+    config.line(
+      `import { configDefaults, defineConfig } from "vitest/config";`,
+    );
     config.line(``);
     config.open(`export default defineConfig({`);
     config.open(`test: {`);
     config.line(`includeSource: ["src/**/*.{ts,tsx}"],`);
+    config.open(`coverage: {`);
+    config.line(
+      `exclude: [...(configDefaults.coverage.exclude ?? []), "lib/**", "coverage/**"],`,
+    );
+    config.close(`},`);
     config.close(`},`);
     config.close(`});`);
     config.line(``);
@@ -27,15 +39,6 @@ export class Vitest extends Component {
   }
 
   preSynthesize(): void {
-    // There's no need to define a local test task if there's a vitest workspace at the root.
-    // The root's test task will use the vitest workspace and run tests for all subprojects.
-    const vitestWorkspace = this.project.root.components.find(
-      (component) => component instanceof VitestWorkspace,
-    ) as VitestWorkspace | undefined;
-    if (vitestWorkspace) {
-      this.project.testTask.reset();
-    }
-
     // Allow tsup, if exists, to treeshake the import.meta.vitest blocks in source files.
     const tsup = this.project.components.find(
       (component) => component instanceof Tsup,
